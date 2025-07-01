7e1e4596fec0008812d7dc22eea5ee0fe396f020
Benjamin Dufour <Benjamin.dufour@cima.ca>
2025-07-01 13:17:32 -0400

    update for the porfolio, not satisfied yet on the buttons and location of sections. need rework

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-//New toggle for light and dark mode. Need to revisit the concept if it's <<efficient or not>>
-//Need to rework the text ... I believe it could be more efficient
+// //New toggle for light and dark mode. Need to revisit the concept if it's <<efficient or not>>
+// //Need to rework the text ... I believe it could be more efficient
 
 let themeSwitchlightmodetxt = "Light Mode"
 let themeSwitchdarkmodetxt = "Dark Mode"
 
-let darkmode = localStorage.getItem("darkmode")
-const themeSwitch = document.getElementById("theme-switch")
-const themeSwitchtxt = document.getElementById("mode-name")
+// let darkmode = localStorage.getItem("darkmode")
+// const themeSwitch = document.getElementById("theme-switch")
+// // const themeSwitchtxt = document.getElementById("mode-name")
 
-const enableDarkmode = () =>{
-    document.body.classList.add("darkmode")
-    localStorage.setItem("darkmode","active")
-    themeSwitchtxt.innerText = themeSwitchdarkmodetxt
-}
+// const enableDarkmode = () =>{
+//     document.body.classList.add("darkmode")
+//     localStorage.setItem("darkmode","active")
+//     document.querySelector("body").classList.toggle("modetransition")
+//     // themeSwitchtxt.innerText = themeSwitchdarkmodetxt
+// }
 
-const disableDarkmode = () =>{
-    document.body.classList.remove("darkmode")
-    localStorage.setItem("darkmode",null)
-    themeSwitchtxt.innerText = themeSwitchlightmodetxt
-}
+// const disableDarkmode = () =>{
+//     document.body.classList.remove("darkmode")
+//     localStorage.setItem("darkmode",null)
+//     document.querySelector("body").classList.toggle(".modetransition_")
+//     // themeSwitchtxt.innerText = themeSwitchlightmodetxt
+// }
 
-if(darkmode === "active") enableDarkmode()
+// if(darkmode === "disable") enableDarkmode()
 
-themeSwitch.addEventListener("click",()=>{
-    darkmode = localStorage.getItem("darkmode")
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+// themeSwitch.addEventListener("click",()=>{
+//     darkmode = localStorage.getItem("darkmode")
+//     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+// })
